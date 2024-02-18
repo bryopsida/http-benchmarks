4208e9523ed7d9ca4c1c21ac627901dcf8883446
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-02-18 16:29:56 -0600

    scaffolding for fastify, express runs in bun and node

---
+++ bun-express/server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+const resp = {
+    test: 'test'
+}
+
+app.get('/', (req, res) => {
+  res.json(resp)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ bun-fastify/server/index.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: false
+})
+
+const resp = {
+    test: 'test'
+}
+
+fastify.get('/', function (request, reply) {
+    reply.send(resp)
+})
+
+// Run the server!
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+})
